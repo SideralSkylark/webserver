ef0afd98404698ba17c61868629bab395f64d931
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-06-05 15:26:06 +0200

    refactor main, for a more idiomatic approuch

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use webserver::server::listner;
 
 fn main() -> anyhow::Result<()> {
-    let result = match listner::listen("1024") {
+    match listner::listen("1024") {
         Ok(o) => o,
         Err(e) => {
             println!("something went wrong {:?}", e);
         }
     };
 
-    Ok(result)
+    Ok(())
 }
